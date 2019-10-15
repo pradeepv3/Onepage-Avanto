fcbcb33ba84bb6d52f7afe2ad38879470311bb1d
vpradeep.velu@gmail.com <vpradeep.velu@gmail.com>
2019-10-15 19:03:00 +0530

    update

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,17 @@ const wait = require('gulp-wait');
 const sourcemaps = require("gulp-sourcemaps");
 const browserSync = require('browser-sync').create();
 const autoprefix = require('gulp-autoprefixer');
+const uglify = require("gulp-uglify");
 
 // SASS File Directory and CSS Destination Directory
 var SassSRC = './assets/sass/*.scss';
 var SassDest = './css/';
+
+
+// SASS File Directory and CSS Destination Directory
+//var JsSRC = './assets/js/*.js';
+var JsSRC = ['./assets/js/*.js', '!./assets/js/plugin/*min.js'];
+var JsDest = './js/';
 
 const paths = {
     sass: './assets/sass/*.scss',
@@ -34,15 +41,26 @@ function style() {
         .pipe(wait(500))
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefix( {overrideBrowserslist: config.styles.browsers} ))
+        .pipe(autoprefix( {overrideBrowserslist: config.styles.browsers, cascade: false} ))
         .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest(SassDest))
         
         .pipe(browserSync.stream());
 }
 
+function compresser() {
+	return gulp.src(JsSRC)
+		.pipe(uglify())
+		//.pipe(gulp.dest(jsDest))
+		// .pipe(rename(function (path) {
+		// 	path.basename += ".min";
+		// }))
+		.pipe(gulp.dest(JsDest))
+}
+
 function watch() {
     gulp.watch(SassSRC, style);
+    //gulp.watch(JsSRC, compresser);
 }
 
 gulp.task('default', watch)
